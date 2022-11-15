4644adaf40174a34c843d1f7cbecc4f1e86dc3c0
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2022-11-15 11:16:36 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // Interactivity of Rating elements. Ability to choose one
 
 let numbers = document.querySelectorAll(`p[class="circle"]`);
-let value;
+let rating;
 
 numbers.forEach(number => number.onclick = () => {
-  if (number.getAttribute("state") == 0) {
+  if (number.getAttribute("pressed") == 0) {
     try {
-      let oldElement = document.querySelector('[state="1"]');
-      oldElement.setAttribute('state', 0);
+      let oldElement = document.querySelector('[pressed="1"]');
+      oldElement.setAttribute('pressed', 0);
     }
     catch(err) {
       console.log(err)
     }
-    number.setAttribute("state", 1)
-    value = number.getAttribute("value");
+    number.setAttribute("pressed", 1)
+    rating = number.getAttribute("rating");
   }
-  else if (number.getAttribute("state") == 1) {
-    number.setAttribute("state", 0)
-    value = undefined;
+  else if (number.getAttribute("pressed") == 1) {
+    number.setAttribute("pressed", 0)
+    rating = undefined;
   }
-  console.log('VALUE IS', value)
-  return value;
+  console.log('Rating IS', rating)
+  return rating;
 })
 
 // Clicking "Submit" - Taking the stored "RATING" and changing the CONTAINER element to "Thank You" State
 
 let submitBtn = document.getElementById('submit')
 submitBtn.onclick = () => {
-  if (value == undefined) return;
+  if (rating == undefined) return;
   let container = document.getElementById('state1')
   let container2 = document.getElementById('state2')
   container.innerHTML = container2.innerHTML
-  let rating = document.getElementById('value');
-  rating.innerText = value;
+  let rating = document.getElementById('rating');
+  rating.innerText = rating;
   container.setAttribute('style', "text-align: center;")
 }
