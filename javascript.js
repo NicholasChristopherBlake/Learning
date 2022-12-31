420472295b0e3ad96c7f2cd19ef1eb7e6b1cb950
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2022-12-31 14:39:33 +0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
+const timeLimit = 100;
 
 function timer(seconds) {
   // Clear any existing timers
