2a15989994895d3ec3eed5e2eb84591e09299461
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-01-18 18:16:19 +0600

    1

--- src/features/AddNewProgramModal.tsx
+++ src/features/AddNewProgramModal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import ExerciseList from "./ExerciseList";
 
 function AddNewProgramModal({ visible }: { visible: boolean }) {
--- src/features/ExerciseSetsContent.tsx
+++ src/features/ExerciseSetsContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import WarmupSetList from "./WarmupSetList";
 import AddWarmupSet from "./AddWarmupSet";
 
